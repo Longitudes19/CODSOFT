aa48f9326e03dc88ef1c22767296b8ed7df49be3
Aryan <amanghi19@gmail.com>
2024-01-02 00:13:34 +0530

    basic calculator

---
+++ calculator.py
@@ -0,0 +1,75 @@
+#Simple calculator with basic arithmetic operations using Python
+  
+import math
+
+def add(x,y):
+    return x+y
+
+def subtract(x,y):
+    if(x >= y):
+        return x-y
+    else:
+        return y-x
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    if y != 0:
+      return x/y
+
+    else:
+      return ('Invalid Division')
+
+def exponential(x,y):
+    return x**y
+
+def modulus(x,y):
+    return x%y
+
+def floorDivision(x,y):
+    return x//y
+
+def ceilDivision(x,y):
+    return math.ceil(x/y)
+
+if __name__ == "__main__":
+    print('Simple Calculator\n')
+    choice = int(input('Enter your choice:\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Exponential\n6.Modulus\n7.floorDivision\n8.ceilDivision\n\n'))
+    a = float(input("Enter the 1st number: "))
+    b = float(input("Enter the 2nd number: "))
+
+    result = None
+
+    if choice == 1:
+        result = add(a,b)
+
+    elif choice == 2:  
+        result = subtract(a,b)
+
+    elif choice == 3:
+        result = multiply(a,b)
+
+    elif choice == 4:
+        result = divide(a,b)
+
+    elif choice == 5:
+        result = exponential(a,b)
+
+    elif choice == 6:
+        result = modulus(a,b)
+
+    elif choice == 7:
+        result = floorDivision(a,b)
+
+    elif choice == 8:
+        result = ceilDivision(a,b)
+
+    else:
+        print('Invalid choice')
+  
+    if result is not None:
+        print('Result:', result)
+
+
+
