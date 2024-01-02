d23f7f86287efe6818357f661b11b7c9ace344c1
Aryan <amanghi19@gmail.com>
2024-01-03 02:53:54 +0530

    calculator commit

--- calculator10.py
+++ calculator10.py
@@ -1,6 +1,7 @@
 #Simple calculator with basic arithmetic operations using Python
   
 import math
+import time
 
 def add(x,y):
     return x+y
@@ -34,42 +35,49 @@ def ceilDivision(x,y):
     return math.ceil(x/y)
 
 if __name__ == "__main__":
-    print('Simple Calculator\n')
-    choice = int(input('Enter your choice:\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Exponential\n6.Modulus\n7.floorDivision\n8.ceilDivision\n\n'))
-    a = float(input("Enter the 1st number: "))
-    b = float(input("Enter the 2nd number: "))
+    while True:
+     print("Simple Calculator\n")
+     print("1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Exponential\n6.Modulus\n7.floorDivision\n8.ceilDivision\n\n")
+     choice = int(input("Enter your choice: "))
+     
+     if 1<= choice <= 8:
+      a = float(input("Enter the 1st number: "))
+      b = float(input("Enter the 2nd number: "))
 
-    result = None
+      result = None
 
-    if choice == 1:
+      if choice == 1:
         result = add(a,b)
 
-    elif choice == 2:  
+      elif choice == 2:  
         result = subtract(a,b)
 
-    elif choice == 3:
+      elif choice == 3:
         result = multiply(a,b)
 
-    elif choice == 4:
+      elif choice == 4:
         result = divide(a,b)
 
-    elif choice == 5:
+      elif choice == 5:
         result = exponential(a,b)
 
-    elif choice == 6:
+      elif choice == 6:
         result = modulus(a,b)
 
-    elif choice == 7:
+      elif choice == 7:
         result = floorDivision(a,b)
 
-    elif choice == 8:
+      elif choice == 8:
         result = ceilDivision(a,b)
+        
+      print('Result:', result)
+      time.sleep(1)
 
-    else:
+     else:
         print('Invalid choice')
+        break
   
-    if result is not None:
-        print('Result:', result)
+    
 
 
 
